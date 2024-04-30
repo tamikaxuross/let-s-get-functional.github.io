15fcb9ef420323381045b0b107ba1aaf6fc761aa
tamikaxuross <tamika.ross.xu@gmail.com>
2024-04-30 08:54:08 -0500

    update

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -16,46 +16,135 @@ var _ = require('underbar');
  *
  * 4. To test your work, run the following command in your terminal:
  *
- *    npm start --prefix ./let-s-get-functional.github.io/projects/let-s-get-functional
+ *    npm start --prefix ./let-s-get-functional.github.io/projects/let-s-get-functional    
  *
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
 var maleCount = function(array) {
-  let males = array.filter(customer => customer.gender === 'male')
-  return males.length;
-};
+    let males = array.filter(customer => customer.gender === 'male')
+    return males.length;
+  };
+  
+  var femaleCount = function(array){
+      let females = array.reduce ((acc, current) => {
+          if (current.gender === 'female'){
+              acc++;
+          }
+          return acc;
+      }, 0);
+  
+      return females;
+  };
+  
+  var oldestCustomer = function(array) {
+      let oldestAge = 0;
+      let oldestName = '';
+      array.forEach(customer => {
+          if (customer.age > oldestAge) {
+              oldestAge = customer.age;
+              oldestName = customer.name;
+          }
+      });
+      
+      return oldestName;
+  };
+  
+  
+  var youngestCustomer = function(customers) {
+      if (!Array.isArray(customers) || customers.length === 0) {
+          return "No customers";
+      }
+  
+      let youngestAge = Infinity;
+      let youngestName = "";
+  
+      customers.forEach(customer => {
+          if (customer.age < youngestAge) {
+              youngestAge = customer.age;
+              youngestName = customer.name;
+          }
+      });
+  
+      return youngestName;
+  };
+  
+  var averageBalance= function(array) {
+      let totalBalance = array.reduce((acc, customer) => acc + Number(customer.balance.replace(/[$,]/g,'')), 0);
+      let average = totalBalance / array.length;
+      return average;
+  };
+  console.log(averageBalance(customers));
 
-var femaleCount = function(array){
-    let females = array.reduce ((acc, current) => {
-        if (current.gender === 'female'){
-            acc++;
+  var firstLetterCount= function(array, letter) {
+      letter = letter.toLowerCase();
+      return array.filter(customer => customer.name.toLowerCase().startsWith(letter)).length;
+  };
+  ;
+  
+  var friendFirstLetterCount = function(array, customer, letter) {
+    let friendsFirstLetter;
+      for ( let i = 0; i < array.length; i++ ) {
+        if ( array[i].name === customer ) {
+          friendsFirstLetter = _.filter(array[i].friends, elem => elem.name[0].toLowerCase() === letter.toLowerCase()).length;
         }
-        return acc;
-    }, 0);
+      }
+      return friendsFirstLetter;
+  };
 
-    return females;
-};
-
-var oldestCustomer = function(array) {
+  
+  var friendsCount = function(array, name) {
+    let count = [];
+    for ( let i = 0; i < array.length; i++ ) {
+        for ( let n = 0; n < array[i].friends.length; n++ ) {
+            if( array[i].friends[n].name === name) {
+                count.push(array[i].name);
+            }
+        }
+    }
+return count;
     
-}
-;
-
-var youngestCustomer;
-
-var averageBalance;
-
-var firstLetterCount;
-
-var friendFirstLetterCount;
-
-var friendsCount;
-
-var topThreeTags;
-
-var genderCount;
-
+};
+  
+  
+  
+  var topThreeTags = function(array) {
+      let tagsCount = {};
+      
+      array.forEach(customer => {
+          customer.tags.forEach(tag => {
+              tagsCount[tag] = (tagsCount[tag] || 0) + 1;
+          });
+      });
+      
+      const sortedTags = Object.keys(tagsCount).sort((a, b) => tagsCount[b] - tagsCount[a]);
+      
+      return sortedTags.slice(0, 3);
+  };
+  
+  var genderCount = function(array) {
+      if (!Array.isArray(array) || array.length === 0) {
+          return "Invalid input or empty array";
+      }
+  
+      let genderCounts = {
+          male: 0,
+          female: 0,
+          'non-binary': 0
+      };
+  
+      array.forEach(person => {
+          if (person.gender === 'male') {
+              genderCounts.male++;
+          } else if (person.gender === 'female') {
+              genderCounts.female++;
+          } else {
+              genderCounts['non-binary']++;
+          }
+      });
+  
+      return genderCounts;
+  }
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
